558a59fa3f37015880172d335b7c6e34c5e0ecc2
Anirban Bhattacharya <anirbanbhattacharya1234@yahoo.co.in>
2015-04-14 15:29:23 -0500

    stable one

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
-
-
-exports.throwExcel= function(data,res){
+exports.throwExcel= function(header,data,res){
+var body="<table>";
+var keys = Object.keys(header);
+ body+="<tr>";
+for(var j=0;j<keys.length;j++)
+{
+  
+ body+="<th>"+header[keys[j]]+"</th>";
+ 
+ }
+body+="</tr>";
+for(var i=0;i<data.length;i++)
+{
+body+="<tr>";
+  for(var j=0;j<keys.length;j++)
+{
+  
+ body+="<td>"+(data[i][keys[j]])+"</td>";
+ 
+ }
+body+="</tr>";
+}
+body+="</table>";
 
 console.log('Converting and exporting to Excel');
 res.writeHead(200, {'Content-Type': 'application/excel'});
-  res.end(data);
+  res.end(body);
 
 
 }
